57ed5fd00df131c2e02734afc92757e2f1f9e255
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2019-03-18 22:11:39 -0400

    added side nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { A11yModule } from '@angular/cdk/a11y';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    A11yModule,
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
